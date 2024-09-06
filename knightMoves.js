27728dda88bc141f5318e391e8218aac8799f1b8
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-09-06 23:10:26 +0400

    Experiment with implementing search function

--- knightMoves.js
+++ knightMoves.js
@@ -17,11 +17,11 @@ function possibleMoves(x, y) {
         for (let j = -2; j <= 2; j += 4) {
             let move = [x + i, y + j];
             if (move[0] <= 7 && move[0] >= 0 && move[1] <= 7 && move[1] >= 0) {
-                movesArray.push(move);
+                movesArray.push(coordinateConverter(...move));
             }
             move = [x + j, y + i];
             if (move[0] <= 7 && move[0] >= 0 && move[1] <= 7 && move[1] >= 0) {
-                movesArray.push(move);
+                movesArray.push(coordinateConverter(...move));
             }
         }
     }
@@ -36,5 +36,43 @@ function adjacencyList() {
     return list;
 }
 
+const list = adjacencyList();
 
-console.log(adjacencyList());
+// function recursiveSearch(startIdx, targetIdx) {
+    // for (let i = 0; i < list[startIdx].length; i++) {
+    //     if (movesList.includes(list[startIdx][i])) {
+    //         continue;
+    //     }
+    //     console.log(list[startIdx][i], targetIdx);
+    //     if (list[startIdx][i] === targetIdx) {
+    //         movesList.push(list[startIdx][i]);
+    //         return movesList;
+    //     }
+    //     movesList.push(list[startIdx][i]);
+    //     recursiveSearch(list[startIdx][i], targetIdx, movesList);
+    // }
+// }
+
+function knightMoves(start, target) {
+    const startIdx = coordinateConverter(...start);
+    const targetIdx = coordinateConverter(...target);
+    const movesList = [];
+    const queue = [startIdx];
+    let currIdx;
+    while (queue.length !== 0) {
+        currIdx = queue.shift();
+        movesList.push(currIdx);
+        for (let i = 0; i < list[currIdx].length; i++) {
+            if (movesList.includes(list[currIdx][i])) {
+                continue;
+            }
+            if (list[currIdx][i] === targetIdx) {
+                movesList.push(list[currIdx][i]);
+                return movesList.map((idx) => indexConverter(idx));
+            }
+            queue.push(list[currIdx][i]);
+        }
+    }
+}
+
+console.log(knightMoves([0, 0], [7, 7]));
