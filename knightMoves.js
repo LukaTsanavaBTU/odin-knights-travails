8f37d0e09deddf654a70b5669b05ad170430f62e
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-09-06 21:44:22 +0400

    Create helper functions: coordinateConverter, indexConverted and possibleMoves

---
+++ knightMoves.js
@@ -0,0 +1,31 @@
+function coordinateConverter(x, y) {
+    return x + y * 8;
+}
+
+function indexConverter(i) {
+    let x = i % 8;
+    let y = (i - x) / 8;
+    return [x, y];
+}
+
+function possibleMoves(x, y) {
+    if (x > 7 || x < 0 || y > 7 || y < 0) {
+        throw new Error("Coordinates out of range");
+    }
+    const movesArray = [];
+    for (let i = -1; i <= 1; i += 2) {
+        for (let j = -2; j <= 2; j += 4) {
+            let move = [x + i, y + j];
+            if (move[0] <= 7 || move[0] >= 0 || move[1] <= 7 || move[1] >= 0) {
+                movesArray.push(move);
+            }
+            move = [x + j, y + i];
+            if (move[0] <= 7 || move[0] >= 0 || move[1] <= 7 || move[1] >= 0) {
+                movesArray.push(move);
+            }
+        }
+    }
+    return movesArray; 
+}
+
+console.log(possibleMoves(3, 2));
