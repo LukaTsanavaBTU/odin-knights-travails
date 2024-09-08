d9a68063a8a8f8549bdf88be229ae0354435c287
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-09-08 21:06:04 +0400

    Tidy up file

--- knightMoves.js
+++ knightMoves.js
@@ -65,8 +65,7 @@ const knightMoves = (function() {
 })();
 
 
-
 console.log(knightMoves([0,0],[1,2]));
 console.log(knightMoves([0,0],[3,3]));
-console.log(knightMoves([3,3],[0,0]));
+console.log(knightMoves([3,3],[0,0]))
 console.log(knightMoves([0, 0], [7, 7]));
